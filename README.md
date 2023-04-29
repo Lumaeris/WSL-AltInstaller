11514c22f6929e26cc9c309154691c6496ab7172
Ilya Prokopenko <dsrev@protonmail.com>
2023-04-29 23:51:45 +0700

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # [WIP] WSL-AltInstaller
+
+This script installs WSL 2 and Ubuntu to the system after system ["amelioration"](https://ameliorated.io) (or if "wsl --install" does not work for some reason).
+
+```powershell
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+irm github.com/Lumaeris/WSL-AltInstaller/raw/main/install.ps1 | iex
+```
+
+## Known issues
+- aria2 downloads with a maximum of 5 threads, even if I set it to 16.
+- "The service has not been started" appeared during testing for no reason. I was installing WSL and Ubuntu in about the same way the script does. I need to perform some testing...
+- **Very** limited functionality.
--- install.ps1
+++ install.ps1
@@ -13,6 +13,11 @@ if([System.Environment]::OSVersion.Version.Build -lt 19041) {
 	exit 1
 }
 
+if(!((Get-CimInstance Win32_operatingsystem).OSArchitecture -eq "64-bit")) {
+	[console]::error.writeline("WSL-AltInstaller: This script works only on a 64-bit Windows system.")
+	exit 1
+}
+
 if([System.Environment]::UserName -eq "Administrator") {
 	# This can usually happen in AME10/11, VERY rarely by the user's own intent (which is stupid IMO).
 	[console]::error.writeline("WSL-AltInstaller: You literally run this script as 'Administrator', not as your personal account. WSL and a distribution of your choice will only be installed on this system account and your personal account will not be able to run these applications.")
@@ -33,18 +38,56 @@ if(!(is_elevated)) {
 	exit 1
 }
 
+if(!((Get-NetConnectionProfile).IPv4Connectivity -contains "Internet" -or (Get-NetConnectionProfile).IPv6Connectivity -contains "Internet")) {
+	[console]::error.writeline("WSL-AltInstaller: This script requires a stable Internet connection.")
+	exit 1
+}
+
 ### END: Various checks
 
+$prevdir = (Get-Location).Path
+$tempdir = [System.IO.Path]::GetTempPath()
+Set-Location $tempdir
+New-Item -Path "WSL-AltInstaller" -ItemType "directory" -Force > $null
+Set-Location "WSL-AltInstaller"
+
 if ((Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform).State -ne 'Enabled'){
-   	"Enabling 'Virtual Machine Platform'..."
-    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName VirtualMachinePlatform -LimitAccess
+	"Enabling 'Virtual Machine Platform'..."
+    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName VirtualMachinePlatform -LimitAccess > $null
 } else {
     "Virtual Machine Platform was already enabled."
 }
 
 if ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -ne 'Enabled'){
     "Enabling 'Windows Subsystem for Linux'..."
-    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux -LimitAccess
+    Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux -LimitAccess > $null
 } else {
     "Windows Subsystem for Linux was already enabled."
 }
+
+$aria2 = Invoke-RestMethod -Uri https://api.github.com/repos/aria2/aria2/releases/latest | Select-Object -ExpandProperty assets | Select-Object -expand browser_download_url
+$aria2link = $aria2[2]
+Invoke-WebRequest $aria2link -OutFile "aria2.zip"
+Expand-Archive .\aria2.zip
+Move-Item "aria2\*\aria2c.exe" .
+Remove-Item -LiteralPath "aria2" -Force -Recurse
+Remove-Item -LiteralPath "aria2.zip"
+
+if(!((Get-AppPackage).Name -like "*WindowsSubsystemForLinux*")) {
+	$wsl = Invoke-RestMethod -Uri https://api.github.com/repos/microsoft/WSL/releases
+	$wsllink = $wsl[0] | Select-Object -ExpandProperty assets | Select-Object -expand browser_download_url
+
+	& ".\aria2c.exe" "-x16" "-k4M" $wsllink "-o" "wsl.msixbundle"
+	Add-AppxPackage "wsl.msixbundle"
+}
+
+if(!((Get-AppPackage).Name -like "*Ubuntu*")) {
+	& ".\aria2c.exe" "-x16" "-k4M" "https://aka.ms/wslubuntu" "-o" "ubuntu.appxbundle"
+	Add-AppxPackage "ubuntu.appxbundle"
+}
+
+Set-Location ..
+Remove-Item -LiteralPath "WSL-AltInstaller" -Force -Recurse
+Set-Location $prevdir
+
+"WSL-AltInstaller: The requested operation is successful (hopefully). Changes will not be effective until the system is rebooted."
